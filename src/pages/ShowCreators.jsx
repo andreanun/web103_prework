cc6f93fe5b66de562dd46aa95cac49ead0d00041
Andrea Nunoo <123588571+andreanun@users.noreply.github.com>
2026-05-02 20:11:51 -0400

    ShowCreators card

--- src/pages/ShowCreators.jsx
+++ src/pages/ShowCreators.jsx
@@ -1,10 +1,37 @@
-// {creators.map((item) => (
-//   <CreatorCard
-//     key={item.id}
-//     id={item.id}
-//     name={item.name}
-//     url={item.url}
-//     description={item.description}
-//     imageURL={item.imageURL}
-//   />
-// ))}
+import { supabase } from "../client";
+import CreatorCard from "../components/CreatorCard";
+import { useState, useEffect } from "react";
+
+const ShowCreators = () => {
+  const [creators, setCreators] = useState([]);
+
+  useEffect(() => {
+    const fetchCreators = async () => {
+      const { data, error } = await supabase
+        .from("creators")
+        .select()
+        .order("created_at", { ascending: false });
+
+      if (error) {
+        console.error("Error fetching creators: ", error);
+      } else {
+        setCreators(data);
+      }
+    };
+    fetchCreators();
+  }, []);
+
+  return (
+    <maim className="container">
+      <div className="grid">
+        {creators && creators.length > 0 ? (
+          creators.map((item) => <CreatorCard key={item.id} {...item} />)
+        ) : (
+          <h2>No creators found. Time to add some!</h2>
+        )}
+      </div>
+    </maim>
+  );
+};
+
+export default ShowCreators;

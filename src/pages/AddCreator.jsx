3a45b023a1d2e773d38e86dd28d39b48de948764
Andrea Nunoo <123588571+andreanun@users.noreply.github.com>
2026-05-04 19:48:48 -0400

    added AddCreator component

---
+++ src/pages/AddCreator.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import { supabase } from "../client";
+import { useNavigate } from "react-router-dom";
+
+const AddCreator = () => {
+  const [creator, setCreator] = useState({
+    name: "",
+    url: "",
+    description: "",
+    imageURL: "",
+  });
+  const navigate = useNavigate();
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setCreator((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    // insert the data into supabase
+    const { error } = await supabase.from("creators").insert([creator]);
+
+    if (error) {
+      console.log(error);
+    } else {
+      navigate("/"); // send  user back home after successful add
+    }
+  };
+
+  return (
+    <main className="container">
+      <h1>Add a New Creator</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="e.g. Ji Ye-eun"
+          onChange={handleChange}
+          required
+        />
+
+        <label htmlFor="url">Channel URL</label>
+        <input
+          type="url"
+          id="url"
+          name="url"
+          placeholder="https://youtube.com/..."
+          onChange={handleChange}
+          required
+        />
+
+        <label htmlFor="description">Description</label>
+        <textarea
+          id="description"
+          name="description"
+          placeholder="What makes them special?"
+          onChange={handleChange}
+          required
+        ></textarea>
+
+        <label htmlFor="imageURL">Image URL (Optional)</label>
+        <input
+          type="text"
+          id="imageURL"
+          name="imageURL"
+          placeholder="Link to a photo..."
+          onChange={handleChange}
+        />
+
+        <button type="submit">Submit Creator</button>
+      </form>
+    </main>
+  );
+};
+
+export default AddCreator;
